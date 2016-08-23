c427c41b2893b5419967792582df12975d93ab46
waston <719254212@qq.com>
2016-08-23 11:36:06 +0800

    first submit

---
+++ TGui.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TGui : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("first submit 1");
+        Debug.Log("first submit 2");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
